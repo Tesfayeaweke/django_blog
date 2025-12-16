22780b7ad647be20bbbdd8e54edf8f09fd84b27d
Tesfaye Eshetu <tesfayee57@gmail.com>
2025-12-16 19:50:46 +0300

    created templates and views for about and home pages.

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,12 +1,39 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
+data = [
+        {
+            "id": 1,
+            "title": "Getting Started with Django",
+            "author": "Tesfaye",
+            "content": "Django makes it easier to build web apps quickly...",
+            "published": "2025-12-01"
+        },
+        {
+            "id": 2,
+            "title": "Understanding URL Routing",
+            "author": "Tesfaye",
+            "content": "URL routing in Django is modular and powerful...",
+            "published": "2025-12-05"
+        },
+        {
+            "id": 3,
+            "title": "Database Design Tips",
+            "author": "Tesfaye",
+            "content": "A clean schema design saves headaches later...",
+            "published": "2025-12-10"
+        }
+    ]
+context = {'post': data, 'page_title': 'blog'}
+    
+
 
 def home(request):
-    return HttpResponse('<h1>Hello Welcome!</h1>')
+    return render(request,'blog/home.html', context)
 def about(request):
-    return HttpResponse('<h1>About Page</h1>')
+    return render(request,'blog/about.html', {'data':'render_test'})
 
 
     
