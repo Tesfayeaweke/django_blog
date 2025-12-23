3a2bd8ea49dd85fe12a5e9f23753026610b418b0
Tesfaye Eshetu <tesfayee57@gmail.com>
2025-12-23 12:03:11 +0300

    created a model, integrated it with views.py and registered it in admins.py to view the model in admins UI.

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,35 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 
 # Create your views here.
-data = [
-        {
-            "id": 1,
-            "title": "Getting Started with Django",
-            "author": "Tesfaye",
-            "content": "Django makes it easier to build web apps quickly...",
-            "published": "2025-12-01"
-        },
-        {
-            "id": 2,
-            "title": "Understanding URL Routing",
-            "author": "Tesfaye",
-            "content": "URL routing in Django is modular and powerful...",
-            "published": "2025-12-05"
-        },
-        {
-            "id": 3,
-            "title": "Database Design Tips",
-            "author": "Tesfaye",
-            "content": "A clean schema design saves headaches later...",
-            "published": "2025-12-10"
-        }
-    ]
-context = {'post': data, 'page_title': 'blog'}
-    
-
-
+context = {'post': Post.objects.all(), 'page_title': 'blog'}
 def home(request):
     return render(request,'blog/home.html', context)
 def about(request):
